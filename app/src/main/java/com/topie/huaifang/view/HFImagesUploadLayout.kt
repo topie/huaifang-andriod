5fc83a64b838d05b53f6da575527d9b4a9eee8dc
arman <bigger007@vip.163.com>
2017-10-20 00:37:01 +0800

    【其他】图片回收问题

---
+++ app/src/main/java/com/topie/huaifang/view/HFImagesUploadLayout.kt
@@ -0,0 +1,16 @@
+package com.topie.huaifang.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.RelativeLayout
+
+/**
+ * Created by arman on 2017/10/20.
+ * 上传图片的视图UI
+ */
+class HFImagesUploadLayout(context: Context?, attrs: AttributeSet? = null, defStyleAttr: Int = -1) : RelativeLayout(context, attrs, defStyleAttr) {
+
+    init {
+
+    }
+}
