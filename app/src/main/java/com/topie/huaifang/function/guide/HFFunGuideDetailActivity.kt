76f0b647bef26aa51038ceaa54debb948704e430
arman <bigger007@vip.163.com>
2017-10-19 22:54:35 +0800

    【其他】办事指南详情

--- app/src/main/java/com/topie/huaifang/function/guide/HFFunGuideDetailActivity.kt
+++ app/src/main/java/com/topie/huaifang/function/guide/HFFunGuideDetailActivity.kt
@@ -2,8 +2,12 @@ package com.topie.huaifang.function.guide
 
 import android.os.Bundle
 import android.text.Html
+import android.widget.LinearLayout
+import android.widget.TextView
 import com.topie.huaifang.R
 import com.topie.huaifang.base.HFBaseTitleActivity
+import com.topie.huaifang.extensions.kFindViewById
+import com.topie.huaifang.extensions.kInflate
 import com.topie.huaifang.extensions.kInto
 import com.topie.huaifang.http.HFRetrofit
 import com.topie.huaifang.http.bean.function.HFFunGuideListResponseBody
@@ -36,7 +40,22 @@ class HFFunGuideDetailActivity : HFBaseTitleActivity() {
         tv_fun_guide_title.text = data?.title
         tv_fun_guide_time.text = data?.publishTime
         tv_fun_guide_publisher.text = data?.publishUser
-        tv_fun_guide_content.text = data?.allContent?.let { Html.fromHtml(it) }
+        //内容
+        ll_fun_guide_content.removeAllViews()
+        val obj = { parent: LinearLayout, subject: String, content: String ->
+            val kInflate = parent.kInflate(R.layout.function_guide_detail_item)
+            kInflate.kFindViewById<TextView>(R.id.tv_fun_guide_detail_subject).text = subject
+            kInflate.kFindViewById<TextView>(R.id.tv_fun_guide_detail_content).text = Html.fromHtml(content)
+            parent.addView(kInflate)
+        }
+        obj(ll_fun_guide_content, "办事条件：", data?.actionCondition ?: "")
+        obj(ll_fun_guide_content, "办事材料：", data?.actionMaterial ?: "")
+        obj(ll_fun_guide_content, "办事地址：", data?.actionAddress ?: "")
+        obj(ll_fun_guide_content, "流程：", data?.actionFlow ?: "")
+        obj(ll_fun_guide_content, "时间范围：", (data?.actionBegin ?: "") + " - " + (data?.actionEnd ?: ""))
+        obj(ll_fun_guide_content, "办事费用：", data?.actionFee ?: "")
+        obj(ll_fun_guide_content, "办事依据：", data?.actionYiju ?: "")
+
     }
 
     override fun onResume() {
