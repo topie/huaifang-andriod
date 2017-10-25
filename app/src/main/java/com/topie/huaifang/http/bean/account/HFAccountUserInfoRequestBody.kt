28244a633533fc179c87bb0df0f12df13c876b9e
arman <bigger007@vip.163.com>
2017-10-25 23:21:06 +0800

    【功能】刷新用户身份

--- app/src/main/java/com/topie/huaifang/http/bean/account/HFAccountUserInfoRequestBody.kt
+++ app/src/main/java/com/topie/huaifang/http/bean/account/HFAccountUserInfoRequestBody.kt
@@ -4,4 +4,4 @@ package com.topie.huaifang.http.bean.account
  * Created by arman on 2017/10/24.
  * 个人信息编辑
  */
-class HFAccountUserInfoRequestBody(val headImage: String?, nickName: String?)
+class HFAccountUserInfoRequestBody(val headImage: String?, val nickName: String?)
