eaeded6822fdc558c956e957955e87a79455efd3
arman <bigger007@vip.163.com>
2017-11-12 15:15:34 +0800

    【FixBug】第二次修改bug

--- app/src/main/java/com/topie/huaifang/extensions/HFBitmapEx.kt
+++ app/src/main/java/com/topie/huaifang/extensions/HFBitmapEx.kt
@@ -1,9 +1,11 @@
 package com.topie.huaifang.extensions
 
+import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Matrix
 import android.media.ExifInterface
+import android.provider.MediaStore
 import java.io.*
 
 
@@ -72,6 +74,13 @@ fun kCompress(file: File, outputFile: File, targetWidth: Int, targetHeight: Int)
             inputStream.kSafeClose()
         }
     }?.let {
+        val orientation = appContext?.let { getOrientation(it, file) } ?: 0
+        when (orientation) {
+            0 -> it
+            else -> it.kRotate((orientation).toFloat())
+
+        }
+    }?.let {
         var outputStream: OutputStream? = null
         try {
             outputStream = FileOutputStream(outputFile)
@@ -84,7 +93,6 @@ fun kCompress(file: File, outputFile: File, targetWidth: Int, targetHeight: Int)
         }
     } ?: false
 }
-
 
 fun getInSampleSize(outWidth: Int, outHeight: Int, targetWidth: Int, targetHeight: Int): Int {
     val sampleSize = when {
@@ -107,26 +115,43 @@ fun getInSampleSize(outWidth: Int, outHeight: Int, targetWidth: Int, targetHeigh
     return sampleSize
 }
 
-/**
- * 获取角度
- *
- * @param path
- * @return
- */
-fun kReadPictureDegree(path: String): Int {
-    return try {
-        val exifInterface = ExifInterface(path)
-        val orientation = exifInterface.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL)
-        when (orientation) {
-            ExifInterface.ORIENTATION_ROTATE_90 -> 90
-            ExifInterface.ORIENTATION_ROTATE_180 -> 180
-            ExifInterface.ORIENTATION_ROTATE_270 -> 270
-            else -> 0
-        }
-    } catch (e: IOException) {
-        log("kReadPictureDegree", e)
-        0
+private fun getOrientation(context: Context, file: File): Int {
+    val orientationFromExif = getOrientationFromExif(file)
+    return if (orientationFromExif != 0) {
+        orientationFromExif
+    } else {
+        getOrientationFromContentResolver(context, file)
     }
+}
+
+private fun getOrientationFromExif(file: File): Int {
+    val exif = ExifInterface(file.absolutePath)
+    val orientation = try {
+        exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL)
+    } catch (e: Exception) {
+        ExifInterface.ORIENTATION_NORMAL
+    }
+    return when (orientation) {
+        ExifInterface.ORIENTATION_ROTATE_90 -> 90
+        ExifInterface.ORIENTATION_ROTATE_180 -> 180
+        ExifInterface.ORIENTATION_ROTATE_270 -> 270
+        else -> 0
+    }.also { log("orientation from exif is $it") }
+}
+
+private fun getOrientationFromContentResolver(context: Context, file: File): Int {
+    val selection = "${MediaStore.Images.Media.DATA} = ?"
+    val imageOrderBy = "${MediaStore.Images.Media._ID} DESC"
+    val imageColumns = arrayOf(MediaStore.Images.Media._ID, MediaStore.Images.ImageColumns.ORIENTATION)
+    val cursor = context.contentResolver.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, imageColumns,
+            selection, arrayOf(file.absolutePath), imageOrderBy)
+    return if (cursor.moveToFirst()) {
+        val orientation = cursor.getInt(cursor.getColumnIndex(MediaStore.Images.ImageColumns.ORIENTATION))
+        cursor.close()
+        orientation
+    } else {
+        0
+    }.also { log("orientation from ContentResolver is $it") }
 }
 
 /**
